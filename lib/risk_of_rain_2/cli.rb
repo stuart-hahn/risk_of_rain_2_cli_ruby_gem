5b28538f751def61f1059e7e283c14f0a22bea66
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-10-27 14:46:53 -0400

    add choices and case statement to handle user's input

--- lib/risk_of_rain_2/cli.rb
+++ lib/risk_of_rain_2/cli.rb
@@ -2,17 +2,51 @@ class RiskOfRain2::CLI
   
   def call
     puts "\nWelcome to the Risk of Rain 2 CLI Cheatsheet."
-    puts ""
     puts "What type of information are you looking for?"
+    puts ""
     list_choices
   end
 
   def list_choices
     puts <<~DOC
-    1. Items
-    2. Survivors
-    3. Artifacts
-  DOC
+      1. Items
+      2. Survivors
+      3. Artifacts
+    DOC
+
+    make_choice
+  end
+
+  def make_choice
+    puts "Select a number from the list."
+    input = gets.strip.downcase
+    puts ""
+
+    case input
+    when "1"
+      fetch_items
+    when "2"
+      fetch_survivors
+    when "3"
+      fetch_artifacts
+    when "exit"
+      puts "Womp, womp. See you later."
+    else
+      "You must select a number or type 'exit'"
+      make_choice
+    end
+  end
+
+  def fetch_items
+    puts "Fetching items"
+  end
+
+  def fetch_survivors
+    puts "Fetching survivors"
+  end
+
+  def fetch_artifacts
+    puts "Fetching artifacts"
   end
 
 end
