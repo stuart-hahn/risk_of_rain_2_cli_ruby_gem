bdec918013da2104c3beaebf9d10de50656df553
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-11-03 12:14:53 -0500

    update gemspec to require nokogiri and pry

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,2 @@
-require_relative '../lib/risk_of_rain_2/cli'
-require_relative '../lib/risk_of_rain_2/version'
+# require_relative '../lib/risk_of_rain_2/cli'
+# require_relative '../lib/risk_of_rain_2/version'
